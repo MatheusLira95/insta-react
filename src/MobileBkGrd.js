abe716ce1d162ac7523dc9fc4d4f4034c4ca20bd
Matheus Lira <matheustlira@gmail.com>
2021-04-30 12:16:19 -0300

    MobileBkGrd componentizado finalizado

--- src/MobileBkGrd.js
+++ src/MobileBkGrd.js
@@ -1,12 +1,9 @@
 export default function MobileBkGrd(){
+    const icons = ["home","search-outline","add-circle-outline","heart-outline","person-outline"];
     return (
         <>
             <div class="fundo-mobile">
-                <ion-icon name="home"></ion-icon>
-                <ion-icon name="search-outline"></ion-icon>
-                <ion-icon name="add-circle-outline"></ion-icon>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="person-outline"></ion-icon>
+                {icons.map(i => (<ion-icon name={i}></ion-icon>))}
             </div>
         </>
     );
