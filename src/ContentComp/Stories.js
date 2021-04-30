b8c76a8e01eef6c700c1c5ca58cd1175d6ee10eb
Matheus Lira <matheustlira@gmail.com>
2021-04-30 17:17:10 -0300

    Stories componentizado

--- src/ContentComp/Stories.js
+++ src/ContentComp/Stories.js
@@ -1,84 +1,21 @@
+import Storie from "./Storie";
+
 export default function Stories(){
-    
+    const storiesData = [
+        {imagem: "assets/img/9gag.svg", user: "9gag"},
+        {imagem: "assets/img/meowed.svg", user: "meowed"},
+        {imagem: "assets/img/barked.svg", user: "barked"},
+        {imagem: "assets/img/nathanwpylestrangeplanet.svg", user: "nathanwpylestrangeplanet"},
+        {imagem: "assets/img/wawawicomics.svg", user: "wawawicomics"},
+        {imagem: "assets/img/respondeai.svg", user: "respondeai"},
+        {imagem: "assets/img/filomoderna.svg", user: "filomoderna"},
+        {imagem: "assets/img/memeriagourmet.svg", user: "memeriagourmet"}
+    ];
     return (
         <>
             <div class="stories">
-                        <div class="story">
-                            <div class="imagem">
-                                <img src="assets/img/9gag.svg" />
-                            </div>
-                            <div class="usuario">
-                                9gag
-                            </div>
-                        </div>
-
-                        <div class="story">
-                            <div class="imagem">
-                                <img src="assets/img/meowed.svg" />
-                            </div>
-                            <div class="usuario">
-                                meowed
-                            </div>
-                        </div>
-
-                        <div class="story">
-                            <div class="imagem">
-                                <img src="assets/img/barked.svg" />
-                            </div>
-                            <div class="usuario">
-                                barked
-                            </div>
-                        </div>
-
-                        <div class="story">
-                            <div class="imagem">
-                                <img src="assets/img/nathanwpylestrangeplanet.svg" />
-                            </div>
-                            <div class="usuario">
-                                nathanwpylestrangeplanet
-                            </div>
-                        </div>
-
-                        <div class="story">
-                            <div class="imagem">
-                                <img src="assets/img/wawawicomics.svg" />
-                            </div>
-                            <div class="usuario">
-                                wawawicomics
-                            </div>
-                        </div>
-                        
-                        <div class="story">
-                            <div class="imagem">
-                                <img src="assets/img/respondeai.svg" />
-                            </div>
-                            <div class="usuario">
-                                respondeai
-                            </div>
-                        </div>
-
-                        <div class="story">
-                            <div class="imagem">
-                                <img src="assets/img/filomoderna.svg" />
-                            </div>
-                            <div class="usuario">
-                                filomoderna
-                            </div>
-                        </div>
-
-                        <div class="story">
-                            <div class="imagem">
-                                <img src="assets/img/memeriagourmet.svg" />
-                            </div>
-                            <div class="usuario">
-                                memeriagourmet
-                            </div>
-                        </div>
-
-                        <div class="setinha">
-                        <ion-icon name="chevron-forward-circle"></ion-icon>
-                        </div>
-                    </div>
+                {storiesData.map(storieData => (<Storie image={storieData.imagem} username={storieData.user}/>))}   
+            </div>
         </>
     );
 }
